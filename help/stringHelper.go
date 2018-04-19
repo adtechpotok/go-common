79d57d8d44c2e5362456e657e0eb1ec0fbaa5fe9
p.pilyaev <p.pilyaev@rambler-co.ru>
2018-04-19 15:39:31 +0300

    Перевёл комментарий

--- help/stringHelper.go
+++ help/stringHelper.go
@@ -2,7 +2,7 @@ package help
 
 import "unicode"
 
-//Переводит первую букву в строке в нижний регистр
+//Convert the first letter to lower case
 func LowerCaseFirstLetter(str string) string {
 	for i, v := range str {
 		return string(unicode.ToLower(v)) + str[i+1:]
