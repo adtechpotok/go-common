246aba121f175892091ac719dc1dc9cac7d43499
p.pilyaev <p.pilyaev@rambler-co.ru>
2018-04-27 18:56:59 +0300

    Made fields of struct public

--- http-response/jsonRawResponse.go
+++ http-response/jsonRawResponse.go
@@ -7,18 +7,18 @@ import (
 
 // JSON response for raw data
 type JSONRawResponse struct {
-	body   interface{}
-	status int
+	Body   interface{}
+	Status int
 }
 
 // Render render raw data to response
 func (response *JSONRawResponse) Render(w http.ResponseWriter) {
-	js, err := jsoniter.Marshal(response.body)
+	js, err := jsoniter.Marshal(response.Body)
 	if err != nil {
 		panic(Error{err, response})
 	}
 	w.Header().Set("Content-Type", "application/json; charset=utf-8")
 	w.Header().Set("Access-Control-Allow-Origin", "*")
-	w.WriteHeader(response.status)
+	w.WriteHeader(response.Status)
 	w.Write(js)
 }
