b2f84f50397919ddd1652baa39fe2c1802f58477
p.pilyaev <p.pilyaev@rambler-co.ru>
2018-04-27 18:37:09 +0300

    Add self-return to chain style calls

--- http-response/baseResponse.go
+++ http-response/baseResponse.go
@@ -11,47 +11,63 @@ type BaseResponse struct {
 }
 
 // CustomErrorState turn on CustomErrorState of the response
-func (res *BaseResponse) CustomErrorState(message string, code int) {
+func (res *BaseResponse) CustomErrorState(message string, code int) *BaseResponse {
 	res.setMessage(message)
 	res.setStatus(code)
+
+	return res
 }
 
 // ServerErrorState turn on ServerErrorState of the response
-func (res *BaseResponse) ServerErrorState() {
+func (res *BaseResponse) ServerErrorState() *BaseResponse {
 	res.setMessage(http.StatusText(http.StatusInternalServerError))
 	res.setStatus(http.StatusInternalServerError)
+
+	return res
 }
 
 // BadRequestErrorState turn on BadRequestErrorState of the response
-func (res *BaseResponse) BadRequestErrorState() {
+func (res *BaseResponse) BadRequestErrorState() *BaseResponse {
 	res.setMessage(http.StatusText(http.StatusBadRequest))
 	res.setStatus(http.StatusBadRequest)
+
+	return res
 }
 
 // ForbiddenErrorState turn on ForbiddenErrorState of the response
-func (res *BaseResponse) ForbiddenErrorState() {
+func (res *BaseResponse) ForbiddenErrorState() *BaseResponse {
 	res.setMessage(http.StatusText(http.StatusForbidden))
 	res.setStatus(http.StatusForbidden)
+
+	return res
 }
 
 // TooManyRequestsErrorState turn on TooManyRequestsErrorState of the response
-func (res *BaseResponse) TooManyRequestsErrorState() {
+func (res *BaseResponse) TooManyRequestsErrorState() *BaseResponse {
 	res.setMessage(http.StatusText(http.StatusTooManyRequests))
 	res.setStatus(http.StatusTooManyRequests)
+
+	return res
 }
 
 // NotFoundErrorState turn on NotFoundErrorState of the response
-func (res *BaseResponse) NotFoundErrorState() {
+func (res *BaseResponse) NotFoundErrorState() *BaseResponse {
 	res.setMessage(http.StatusText(http.StatusNotFound))
 	res.setStatus(http.StatusNotFound)
+
+	return res
 }
 
 // setMessage set response message
-func (res *BaseResponse) setMessage(message string) {
+func (res *BaseResponse) setMessage(message string) *BaseResponse {
 	res.Body = message
+
+	return res
 }
 
 // setStatus set response status
-func (res *BaseResponse) setStatus(status int) {
+func (res *BaseResponse) setStatus(status int) *BaseResponse {
 	res.status = status
+
+	return res
 }
