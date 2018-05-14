e0c35283a9df900ebada0e00f37f9e65fa28f86a
p.pilyaev <p.pilyaev@rambler-co.ru>
2018-05-14 19:53:58 +0300

    Теперь у нас есть цепочки!

--- http-response/httpResponse.go
+++ http-response/httpResponse.go
@@ -1,29 +1,71 @@
 package http_response
 
+import (
+	`net/http`
+)
+
 type Error struct {
 	Error    error
 	Response Response
 }
 
-
-/*
-// Very simple response
-type SimpleResponse interface {
-	Render(w http.ResponseWriter)
-}
-*/
-
-/*
 // Regular response
 type Response interface {
 	Render(w http.ResponseWriter)
-	CustomErrorState(message string, code int)
-	ServerErrorState()
-	BadRequestErrorState()
-	ForbiddenErrorState()
-	TooManyRequestsErrorState()
-	NotFoundErrorState()
-	setMessage(message string)
-	setStatus(status int)
+	CustomState(message interface{}, code int) Response
+	ServerErrorState() Response
+	BadRequestErrorState() Response
+	ForbiddenErrorState() Response
+	TooManyRequestsErrorState() Response
+	NotFoundErrorState() Response
+	setMessage(body interface{}) Response
+	setStatus(status int) Response
 }
-*/
+
+// CustomState turn on CustomState of the response
+func CustomState(res Response, message interface{}, code int) Response {
+	res.setMessage(message)
+	res.setStatus(code)
+
+	return res
+}
+
+// ServerErrorState turn on ServerErrorState of the response
+func ServerErrorState(res Response) Response {
+	res.setMessage(http.StatusText(http.StatusInternalServerError))
+	res.setStatus(http.StatusInternalServerError)
+
+	return res
+}
+
+// BadRequestErrorState turn on BadRequestErrorState of the response
+func BadRequestErrorState(res Response) Response {
+	res.setMessage(http.StatusText(http.StatusBadRequest))
+	res.setStatus(http.StatusBadRequest)
+
+	return res
+}
+
+// ForbiddenErrorState turn on ForbiddenErrorState of the response
+func ForbiddenErrorState(res Response) Response {
+	res.setMessage(http.StatusText(http.StatusForbidden))
+	res.setStatus(http.StatusForbidden)
+
+	return res
+}
+
+// TooManyRequestsErrorState turn on TooManyRequestsErrorState of the response
+func TooManyRequestsErrorState(res Response) Response {
+	res.setMessage(http.StatusText(http.StatusTooManyRequests))
+	res.setStatus(http.StatusTooManyRequests)
+
+	return res
+}
+
+// NotFoundErrorState turn on NotFoundErrorState of the response
+func NotFoundErrorState(res Response) Response {
+	res.setMessage(http.StatusText(http.StatusNotFound))
+	res.setStatus(http.StatusNotFound)
+
+	return res
+}
--- http-response/jsonResponse.go
+++ http-response/jsonResponse.go
@@ -1,14 +1,19 @@
 package http_response
 
 import (
-	"github.com/json-iterator/go"
-	"net/http"
+	`net/http`
+	`github.com/json-iterator/go`
 )
 
-/*
-// Struct to JSON response
+// JSON return empty JSONResponse struct
+func JSON() *JSONResponse {
+	return &JSONResponse{}
+}
+
+// JSON response
 type JSONResponse struct {
-	Response
+	Body   string `json:"message"`
+	status int
 }
 
 // RenderJson convert the response to JSON and send it to client
@@ -22,18 +27,45 @@ func (res *JSONResponse) Render(w http.ResponseWriter) {
 	w.WriteHeader(res.status)
 	w.Write(js)
 }
-*/
-func JSONRender(response Response, w http.ResponseWriter) {
-	js, err := jsoniter.Marshal(response)
-	if err != nil {
-		panic(Error{err, response})
-	}
-	w.Header().Set("Content-Type", "application/json; charset=utf-8")
-	w.Header().Set("Access-Control-Allow-Origin", "*")
-	w.WriteHeader(response.status)
-	w.Write(js)
+
+// setStatus set response Status
+func (res *JSONResponse) setStatus(status int) Response {
+	res.status = status
+	return res
 }
 
-func JSON() Response {
-	return Response{renderType: jsonType}
+// setMessage set response message
+func (res *JSONResponse) setMessage(message interface{}) Response {
+	res.Body = message.(string)
+	return res
+}
+
+// CustomState turn on CustomState of the response
+func (res *JSONResponse) CustomState(message interface{}, code int) Response {
+	return CustomState(res, message, code)
+}
+
+// ServerErrorState turn on ServerErrorState of the response
+func (res *JSONResponse) ServerErrorState() Response {
+	return ServerErrorState(res)
+}
+
+// BadRequestErrorState turn on BadRequestErrorState of the response
+func (res *JSONResponse) BadRequestErrorState() Response {
+	return BadRequestErrorState(res)
+}
+
+// ForbiddenErrorState turn on ForbiddenErrorState of the response
+func (res *JSONResponse) ForbiddenErrorState() Response {
+	return ForbiddenErrorState(res)
+}
+
+// TooManyRequestsErrorState turn on TooManyRequestsErrorState of the response
+func (res *JSONResponse) TooManyRequestsErrorState() Response {
+	return TooManyRequestsErrorState(res)
+}
+
+// NotFoundErrorState turn on NotFoundErrorState of the response
+func (res *JSONResponse) NotFoundErrorState() Response {
+	return NotFoundErrorState(res)
 }
